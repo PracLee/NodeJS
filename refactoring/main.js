f653bebcefc1afb3553253a4d78e98538382165c
mrgoga@naver.com <mrgoga@naver.com>
2022-07-31 23:45:42 +0900

    dldl

---
+++ refactoring/main.js
@@ -0,0 +1,9 @@
+// 모든 왕좌의 게임 가문의 캐릭터들을 놓고 봤을 때 가장 부정적인 가문과 가장 긍정적인 가문을 알아보자.
+
+const https = require('https')
+
+const resultsByHouseSlug = {}
+
+https.get(`https://game-of-thrones-quotes.herokuapp.com/v1/houses`, (res) => {
+  let
+})
