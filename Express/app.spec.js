79a47e82378118943467a6b8d6106d05d6100f2a
mrgoga@naver.com <mrgoga@naver.com>
2022-09-06 22:56:21 +0900

    app test

--- Express/app.spec.js
+++ Express/app.spec.js
@@ -5,13 +5,35 @@
 
 // supertest
 const supertest = require('supertest')
-const request = supertest('./app')
+const app = require('./app')
 
-test('our first test', async () => {
-  const result = await request.get('/users/14').accept('application/json')
-  console.log(result.body)
+const request = supertest(app)
+
+test('retrieve user json', async () => {
+  const result = await request.get('/users/15').accept('application/json')
 
   expect(result.body).toMatchObject({
     nickname: expect.any(String),
+  })
+})
+
+test('retrieve user page', async () => {
+  const result = await request.get('/users/15').accept('text/html')
+
+  expect(result.text).toMatch(/^<html>.*<\/html>$/)
+})
+
+test('update nickname', async () => {
+  const newNickname = 'newNickname'
+
+  const res = await request
+    .post('/users/15/nickname')
+    .send({ nickname: newNickname })
+  expect(res.status).toBe(200)
+
+  const userResult = await request.get('/users/15').accept('application/json')
+  expect(userResult.status).toBe(200)
+  expect(userResult.body).toMatchObject({
+    nickname: newNickname,
   })
 })
