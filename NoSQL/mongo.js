bbc1cec1d141a9267e34e807a153cf4fb797531e
mrgoga@naver.com <mrgoga@naver.com>
2022-09-22 21:42:09 +0900

    mongo

--- NoSQL/mongo.js
+++ NoSQL/mongo.js
@@ -1,3 +1,14 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
-const uri = `mongodb+src://dbUser:${process.env.MONGO_PASSWORD}@cluster0.sjdhodksehosl`
+const uri = `mongodb+srv://spsbsc:${process.env.MONGO_PASSWORD}@cluster0.ykmzb2r.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+
+client.connect((err) => {
+  const collection = client.db('test').collection('devices')
+  // perform actions on the collection object
+  client.close()
+})
